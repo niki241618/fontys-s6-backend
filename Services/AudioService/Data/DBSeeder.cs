c2233d8d72b5c22a5eea7b03b1c030977e2dc235
Nikolay Valchanov <n.valchanov@student.fontys.nl>
2024-04-29 13:03:58 +0200

    Updated database seeding to assign id 1 to the Arnold's book

--- Services/AudioService/Data/DBSeeder.cs
+++ Services/AudioService/Data/DBSeeder.cs
@@ -17,19 +17,19 @@ public class DbSeeder
 		dbContext.Database.EnsureCreated();
 		dbContext.Books.AddRange(new BookEntity
 		{
-			Id = 0,
+			Id = 1,
 			Name = "Be Useful: Seven Tools for Life",
 			Authors = new []{ "Arnold Schwarzenegger" },
 			Description = "The seven rules to follow to realize your true purpose in life—distilled by Arnold Schwarzenegger from his own journey of ceaseless reinvention and extraordinary achievement, and available for absolutely anyone.",
 			Language = "English",
 			Genre = "Motivation",
-			Length = 4321,
+			Length = 2132,
 			AudioUri = "4c83c2c9-8307-4ee8-a2fe-32b578bd2d0d.mp3",
 			CoverUri = "https://audiooasisaudiobookstest.blob.core.windows.net/coverimages/4as1a21-2s3seo2d-212a.png"
 		}, 
 		new BookEntity
 		{
-			Id = 1,
+			Id = 2,
 			Name = "The Black Veil",
 			Authors = new []{ "Boz" },
 			Description = "It is the year 1800, or thereabouts, and a young doctor has recently opened his own surgery.  All he is waiting for is his first patient.  However, none seem to be forthcoming.",
@@ -41,7 +41,7 @@ public class DbSeeder
 		},
 		new BookEntity
 		{
-			Id = 2,
+			Id = 3,
 			Name = "The 3 Questions",
 			Authors = new []{ "Leo Tolstoy" },
 			Description = "It once occurred to a certain king that if he always knew the right time to begin everything.",
